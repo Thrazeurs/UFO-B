0f5a3ebdea4769bf7a83138882e9f87fa1f94a48
David Ponce <poncejd@campusemav.com>
2020-10-19 09:59:10 +0200

    Clase 19-10
    pickup

---
+++ Assets/Script/ControlGame.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlGame : MonoBehaviour
+{
+    public static int punts = 0; //puntuació //static = nomes un valor per a tot el joc
+}
---
+++ Assets/Script/Movimimiento.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movimimiento : MonoBehaviour
+{
+    [SerializeField]
+    int velocidad = 3;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x, y;
+        x = Input.GetAxis("Horizontal");
+        y = Input.GetAxis("Vertical");
+        //print("horizontal: " + x + " / vertical: " + y);
+        transform.Translate(x *Time.deltaTime * velocidad, y* Time.deltaTime * velocidad, 1* Time.deltaTime * velocidad);
+    }
+}
